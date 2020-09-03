9e982eb9eda73e2e00060fc6099a417e22dc6734
dkoslicki <dmkoslicki@gmail.com>
2020-09-03 12:43:48 -0700

    initial working python implementtion

--- profiling/scripts/create_OPAL_results.py
+++ profiling/scripts/create_OPAL_results.py
@@ -42,6 +42,7 @@ plotLabel = f"{dataset}, {submission_type}"
 # Look for results to evaluate
 # NOTE! This looks in a specific sub-directory
 to_evaluate = []
+labels = []
 for i in range(len(anonymous_names)):
     anonymous_name = anonymous_names[i]
     file_name = os.path.join(base, "data/submissions", submission_type, anonymous_name + ".profile")
@@ -49,8 +50,13 @@ for i in range(len(anonymous_names)):
     if os.path.exists(file_name):
         #print(anonymous_name)
         to_evaluate.append(file_name)
+        labels.append(f"{tool_names[i]}_{versions[i]}_{i}")  # FIXME: stupid "_{i}" on the end since OPAL wants unique labels
 
 # Delete results if they exist
-#shutil.rmtree()
-print(opal_out)
+shutil.rmtree(opal_out, ignore_errors=True)
+
+# Run OPAL
+to_run = f"{python_exec} {opal_script} -g {ground_truth} -o {opal_out} -d \"{plotLabel}\"" \
+         f"-l \"{','.join(labels)}\" {' '.join(to_evaluate)}"
+res = subprocess.run(to_run, shell=True)
 
