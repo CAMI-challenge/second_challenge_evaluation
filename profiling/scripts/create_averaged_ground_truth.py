38d513d8985a85da4e7ae3c2807ac7706cb8a042
dkoslicki <dmkoslicki@gmail.com>
2020-09-04 11:54:03 -0700

    more towards #7

--- profiling/scripts/create_averaged_ground_truth.py
+++ profiling/scripts/create_averaged_ground_truth.py
@@ -54,4 +54,32 @@ for rank in ranks:
     for tax_id in average_rank_to_tax_id_to_percentage[rank].keys():
         average_rank_to_tax_id_to_percentage[rank][tax_id] = np.sum(average_rank_to_tax_id_to_percentage[rank][tax_id]) / float(num_samples)
 
-# Now to somehow export this
+# Now to export this
+# keep track of tax paths for writing output
+tax_id_to_taxpathsn = dict()
+tax_id_to_taxpath = dict()
+for profile in sample_list:
+    for prediction in profile[2]:
+        tax_id_to_taxpath[prediction.taxid] = prediction.taxpath
+        tax_id_to_taxpathsn[prediction.taxid] = prediction.taxpathsn
+
+# Write header information from one of the files
+if "marine" in dataset:
+    header = """@SampleID:marmgCAMI2_short_read_average
+@Version:0.9.1
+@Ranks:superkingdom|phylum|class|order|family|genus|species|strain
+
+@@TAXID	RANK	TAXPATH	TAXPATHSN	PERCENTAGE"""
+else:
+    header = """@SampleID:strmgCAMI2_short_read_average
+@Version:0.9.1
+@Ranks:superkingdom|phylum|class|order|family|genus|species|strain
+
+@@TAXID	RANK	TAXPATH	TAXPATHSN	PERCENTAGE"""
+
+out_file = ground_truth.split('.')[0] + "_average.profile"
+with open(out_file, 'w') as fid:
+    fid.write(header)
+    for rank in ranks:
+        for (tax_id, percentage) in average_rank_to_tax_id_to_percentage[rank]:
+            fid.write(f"{tax_id}\t{rank}\t{tax_id_to_taxpath[tax_id]}\t{tax_id_to_taxpathsn[tax_id]}\t{percentage}")
