29cc7f6d9405bd965faba30a9453ec01cb6ad1b7
AlphaSquad <afritz@helmholtz-hzi.de>
2021-09-23 19:17:20 +0200

    Added further 16S evaluation

--- assembly/scripts/eval_16S.py
+++ assembly/scripts/eval_16S.py
@@ -20,6 +20,7 @@ for f in files:
     frags = []
     cigs = []
     diffs = []
+    c_lengths = []
     length = 0
     cont = False
     assembler = f[:f.find("Otu") - 1]
@@ -27,6 +28,7 @@ for f in files:
         for line in p:
             spl = line.strip().split('\t')
             length = spl[1]
+            c_lengths.append(int(spl[6]))
             for elem in spl:
                 if elem.startswith("cg:"): #CIGAR
                     cig = elem.rsplit(":",1)[1]
@@ -49,25 +51,33 @@ for f in files:
             assemblers[assembler][0].extend(frags)
             assemblers[assembler][1].extend(cigs)
             assemblers[assembler][2].extend(diffs)
+            assemblers[assembler][3].extend(c_lengths)
         else:
-            assemblers[assembler] = [frags, cigs, diffs]
+            assemblers[assembler] = [frags, cigs, diffs, c_lengths]
 
 subset = ['ABySS_short','A-STAR_contig_hybrid','HipMer_Metagenome_short','marmgCAMI2_short_read_pooled_gsa','Megahit_v1-1-4-2_short','metaSPAdes_v3-13-1_short','Miniasm_GATB_hybrid','OPERA-MS-inhouse_hybrid']
+subset2_no_gsa = ['ABySS_short','A-STAR_contig_hybrid','HipMer_Metagenome_short','Megahit_v1-1-4-2_short','metaSPAdes_v3-13-1_short','Miniasm_GATB_hybrid','OPERA-MS-inhouse_hybrid']
 subset_names = {'ABySS_short':"ABySS",'A-STAR_contig_hybrid':"A-STAR",'HipMer_Metagenome_short':"HipMer",'marmgCAMI2_short_read_pooled_gsa':"gsa",'Megahit_v1-1-4-2_short':"MEGAHIT",'metaSPAdes_v3-13-1_short':"metaSPAdes",'Miniasm_GATB_hybrid':"GATB",'OPERA-MS-inhouse_hybrid':"OPERA-MS"}
 fig, (ax1, ax2) = plt.subplots(figsize=(14, 12), nrows=1, ncols=2, sharey = True)
 ax1.set_title("Completeness")
 ax2.set_title("Divergence")
 completeness = []
 divergence = []
+names = []
+lengths = []
 for assembler in assemblers:
     if assembler not in subset:
         continue
     print(assembler)
     print("mean: %s" % statistics.mean(assemblers[assembler][0]))
     print("median: %s" % statistics.median(assemblers[assembler][0]))
+    print("mean contig length: %s" % statistics.mean(assemblers[assembler][3]))
+    print("median contig length: %s" % statistics.median(assemblers[assembler][3]))
     completeness.append(assemblers[assembler][0])
     divergence.append(assemblers[assembler][2])
-
+    if (assembler != 'marmgCAMI2_short_read_pooled_gsa'):
+        lengths.append(assemblers[assembler][3])
+        names.append(subset_names[assembler])
 
 plot1 = ax1.violinplot(completeness, vert=False)
 plot2 = ax2.violinplot(divergence, vert=False)
@@ -86,3 +96,14 @@ for patch, color in zip(plot2['bodies'], colors):
     patch.set_edgecolor('black')
 fig.suptitle("16S rRNA gene assembly quality", fontsize=15)
 plt.savefig("16S_violins.png")
+
+plot3 = plt.violinplot(lengths, vert=False)
+plt.yticks(np.arange(1, len(subset2_no_gsa) + 1),names)
+#plt.ticklabel_format(axis='x',style='sci',scilimits=(0,0))
+plt.xlabel("Mapped contig lengths")
+colors = sns.color_palette("tab10")
+for patch, color in zip(plot3['bodies'], colors):
+    patch.set_color(color)
+    patch.set_edgecolor('black')
+plt.title("16S rRNA gene assembly contig lengths", fontsize=12)
+plt.savefig("16S_violins_lengths.png", bbox_inches='tight')
