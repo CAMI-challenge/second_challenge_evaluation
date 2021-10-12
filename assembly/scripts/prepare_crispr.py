1d14b90de2eb1db6dfbb4a292dbf81a088143bbf
AlphaSquad <afritz@helmholtz-hzi.de>
2021-10-12 17:19:46 +0200

    Added CRISPR extraction and sequences

--- assembly/scripts/prepare_crispr.py
+++ assembly/scripts/prepare_crispr.py
@@ -1,30 +1,60 @@
 #!/usr/bin/env python
 
 import sys
+import os
+from Bio import SeqIO
 
 crisprs = sys.argv[1] # cami_crisprs.tsv
 acc_mapping = sys.argv[2] # accession_otu_mapping.tsv
 gid_file = sys.argv[3] # genome_to_id.tsv
+out_path = sys.argv[4]
 
 all_crisprs = {}
 with open(crisprs, 'r') as c:
     genome = ""
+    last = ""
     for line in c:
         if line.startswith("GCA"):
-            genome = line.strip()
+            accession = line.strip()
+            if last.startswith("GCA"):
+                pass
+                # No CRISPR found
+                #print(last)
         else:
             start, length, trustlvl, seq = line.strip().split('\t')
             if int(trustlvl) >= 4: # levels from 1 to 4 with 4 most certain
-                if genome in all_crisprs:
-                    all_crisprs[genome].append((start, length, trustlvl, seq))
+                if accession in all_crisprs:
+                    all_crisprs[accession].append((start, length, trustlvl, seq))
                 else:
-                    all_crisprs[genome] = [(start, length, trustlvl, seq)]
+                    all_crisprs[accession] = [(start, length, trustlvl, seq)]
+        last = line.strip()
 
 otu_acc_map = {}
 with open(acc_mapping, 'r') as otu_acc_map_f:
     for line in otu_acc_map_f:
-        genome, otu, accession = line.strip().split('\t')
-        otu_acc_map[accession] = (otu, genome)
+        genome, otu, accession = line.strip().split()
+        otu_acc_map[otu] = (accession, genome)
+
+crispr_map = {}
+with open(gid_file, 'r') as gids:
+    for line in gids:
+        otu, path = line.strip().split('\t')
+        if otu in otu_acc_map:
+            accession, genome = otu_acc_map[otu]
+            if accession in all_crisprs:
+                crisprs = all_crisprs[accession]
+                for record in SeqIO.parse(path, "fasta"):
+                    i = 0
+                    out_file = os.path.join(out_path, "%s_crisprs.fa" % accession)
+                    for crispr in crisprs:
+                        start, length, trust, seq = crispr
+                        crispr_sequence = record.seq[int(start):int(start)+int(length)]
+                        i += 1
+                        with open(out_file, 'a+') as f:
+                            f.write(">%s_CRISPR_%s\n" % (accession, i))
+                            f.write(str(crispr_sequence))
+                            f.write('\n')
+                    break # ignore plasmids etc
 
 i = 0
 j = 0
@@ -33,6 +63,4 @@ for c in all_crisprs:
         i += 1
         j += len(all_crisprs[c])
         
-
-
 print("%s CRISPR sequences for %s genomes (out of 65)" % (j, i))
