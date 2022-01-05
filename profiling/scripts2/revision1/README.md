a4b9e6b38a3f701d3ab242da18edf492d5eabe4e
Fernando Meyer <fernando.meyer@gmail.com>
2022-01-05 11:33:10 -0300

    add unifrac for fresh and 1 week old samples of Qiita project

---
+++ profiling/scripts2/revision1/README.md
@@ -0,0 +1,12 @@
+Descriptive statistics of [SamplesForUniFracBaseline/unifrac_human__fresh_1week.tsv]
+
+| count | 595.000000 |
+| ----- | ---------- |
+| mean  |   0.222951 |
+| std   |   0.165731 |
+| min   |   0.011615 |
+| 25%   |   0.058203 |
+| 50%   |   0.140958 |
+| 75%   |   0.394142 |
+| max   |   0.437265 |
+
--- profiling/scripts2/revision1/unifrac_replicates.py
+++ profiling/scripts2/revision1/unifrac_replicates.py
@@ -17,53 +17,68 @@ import re
 ALL_RANKS = ['superkingdom', 'phylum', 'class', 'order', 'family', 'genus', 'species']
 
 
-table = biom.load_table('cami2/second_challenge_evaluation/profiling/scripts2/revision1/SamplesForUniFracBaseline/79884_otu_table.redist.species.biom')
+def get_samples_from_biom(sample_ids, biom_table):
+    samples_list = []
+    observations = biom_table.ids(axis='observation')
+    for sample_id in sample_ids:
+        profile = []
+        for obs in observations:
+            percentage = float(biom_table.get_value_by_ids(obs_id=obs, samp_id=sample_id))
+            if percentage == 0:
+                continue
 
-samples = table.ids(axis='sample')
+            prediction = load_data.Prediction()
+            metadata = biom_table.metadata(id=obs, axis='observation')
 
-p = re.compile('^10394.H1.*.*.*.II')
-samples = [x for x in samples if p.match(x)]  # list(filter(p.match, samples))
+            taxpathsn_list = [x for x in metadata['taxonomy'] if x[3:]]
+            taxpathsn = '|'.join(taxpathsn_list)
 
-observations = table.ids(axis='observation')
-sampleid_to_profile = {}
-samples_list = []
-sample_ids_combinations = list(itertools.combinations(samples, 2))
+            prediction.taxid = taxpathsn_list[-1]
+            prediction.rank = ALL_RANKS[len(taxpathsn_list) - 1]
+            prediction.percentage = float(biom_table.get_value_by_ids(obs_id=obs, samp_id=sample_id))
 
-for sample_id in samples:
-    profile = []
-    for obs in observations:
-        percentage = float(table.get_value_by_ids(obs_id=obs, samp_id=sample_id))
-        if percentage == 0:
-            continue
+            prediction.taxpath = taxpathsn
+            prediction.taxpathsn = taxpathsn
+            profile.append(prediction)
+        samples_list.append((sample_id, {}, profile))
+    return samples_list
 
-        prediction = load_data.Prediction()
-        metadata = table.metadata(id=obs, axis='observation')
 
-        taxpathsn_list = [x for x in metadata['taxonomy'] if x[3:]]
-        taxpathsn = '|'.join(taxpathsn_list)
+def main():
+    biom_table1 = biom.load_table('second_challenge_evaluation/profiling/scripts2/revision1/SamplesForUniFracBaseline/105260_species.biom')  # SJII_MG_1-4_RKL0006 (7944)
+    biom_table2 = biom.load_table('second_challenge_evaluation/profiling/scripts2/revision1/SamplesForUniFracBaseline/105255_species.biom')  # RKL0006_SJII_5-8_MG_lane1 (7966)
 
-        prediction.taxid = taxpathsn_list[-1]
-        prediction.rank = ALL_RANKS[len(taxpathsn_list) - 1]
-        prediction.percentage = float(table.get_value_by_ids(obs_id=obs, samp_id=sample_id))
+    p = re.compile('^10394\.H1\..*(1week|fresh)')
 
-        prediction.taxpath = taxpathsn
-        prediction.taxpathsn = taxpathsn
-        profile.append(prediction)
-    samples_list.append((sample_id, {}, profile))
-print('done loading')
+    sample_ids1 = list(filter(p.match, biom_table1.ids(axis='sample')))
+    sample_ids2 = list(filter(p.match, biom_table2.ids(axis='sample')))
 
-load_data.normalize_samples(samples_list)
-print('done normalizing')
+    samples_list = get_samples_from_biom(sample_ids1, biom_table1)
+    samples_list += get_samples_from_biom(sample_ids2, biom_table2)
 
-for sample in samples_list:
-    sample_id, sample_metadata, profile = sample
-    sampleid_to_profile[sample_id] = PF.Profile(sample_metadata=sample_metadata, profile=profile)
-print('done 3')
+    sampleid_to_profile = {}
 
-samples12_to_unifrac = {}
-for i, (sample1, sample2) in enumerate(sample_ids_combinations):
-    print(i)
-    samples12_to_unifrac[(sample1, sample2)] = uf.compute_unifrac(sampleid_to_profile[sample1], sampleid_to_profile[sample2])[0]
+    print('done loading')
 
-df = pd.DataFrame.from_dict(samples12_to_unifrac, orient='index')
-df.to_csv('second_challenge_evaluation/profiling/scripts2/revision1/SamplesForUniFracBaseline/79884_unifrac_human.tsv', sep='\t')
+    load_data.normalize_samples(samples_list)
+    print('done normalizing')
+
+    for sample in samples_list:
+        sample_id, sample_metadata, profile = sample
+        sampleid_to_profile[sample_id] = PF.Profile(sample_metadata=sample_metadata, profile=profile)
+    print('done 3')
+
+    sample_ids_combinations = list(itertools.combinations(sample_ids1 + sample_ids2, 2))
+
+    samples12_to_unifrac = {}
+    for i, (sample1, sample2) in enumerate(sample_ids_combinations):
+        print(i)
+        samples12_to_unifrac[(sample1, sample2)] = uf.compute_unifrac(sampleid_to_profile[sample1], sampleid_to_profile[sample2])[0]
+
+    df = pd.DataFrame.from_dict(samples12_to_unifrac, orient='index')
+    df.to_csv('second_challenge_evaluation/profiling/scripts2/revision1/SamplesForUniFracBaseline/unifrac_human__fresh_1week.tsv', sep='\t')
+    print(df.describe())
+
+
+if __name__ == "__main__":
+    main()
