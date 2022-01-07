935b2254e23fb3a0adffdbbe0e92e97f28738d84
AlphaSquad <afritz@helmholtz-hzi.de>
2022-01-07 14:56:46 +0100

    Updated 16S/CRISPR script and plots

--- assembly/scripts/eval_regions.py
+++ assembly/scripts/eval_regions.py
@@ -8,6 +8,7 @@ import seaborn as sns
 import numpy as np
 
 paf = sys.argv[1]
+which = sys.argv[2]
 
 max_diff = 0.
 assemblers = {}
@@ -23,8 +24,10 @@ for f in files:
     c_lengths = []
     length = 0
     cont = False
-    assembler = f[:f.find("Otu") - 1]
-    ##assembler = f[:f.find("GCA") - 1] # for CRISPRs
+    if which == "16S":
+        assembler = f[:f.find("Otu") - 1]
+    else:
+        assembler = f[:f.find("GCA") - 1] # for CRISPRs
     with open(paff, 'r') as p:
         for line in p:
             spl = line.strip().split('\t')
@@ -56,10 +59,12 @@ for f in files:
         else:
             assemblers[assembler] = [frags, cigs, diffs, c_lengths]
 
-subset_names = {'ABySS_short':"ABySS",'A-STAR_contig_hybrid':"A-STAR",'HipMer_Metagenome_short':"HipMer",'marmgCAMI2_short_read_pooled_gsa':"gsa",'Megahit_v1-1-4-2_short':"MEGAHIT",'metaSPAdes_v3-13-1_short':"metaSPAdes",'Miniasm_GATB_hybrid':"GATB",'OPERA-MS-inhouse_hybrid':"OPERA-MS"}
-##subset_names = {'ABySS':"ABySS",'A-STAR':"A-STAR",'HipMer':"HipMer",'marmgCAMI2':"gsa",'Megahit':"MEGAHIT",'metaSPAdes':"metaSPAdes",'Miniasm':"GATB",'OPERA-MS-inhouse':"OPERA-MS"}
-subset = ['ABySS_short','A-STAR_contig_hybrid','HipMer_Metagenome_short','marmgCAMI2_short_read_pooled_gsa','Megahit_v1-1-4-2_short','metaSPAdes_v3-13-1_short','Miniasm_GATB_hybrid','OPERA-MS-inhouse_hybrid']
-##subset = subset_names.keys()
+if which == "16S":
+    subset_names = {'ABySS_short':"ABySS",'A-STAR_contig_hybrid':"A-STAR",'HipMer_Metagenome_short':"HipMer",'marmgCAMI2_short_read_pooled_gsa':"gsa",'Megahit_v1-1-4-2_short':"MEGAHIT",'metaSPAdes_v3-13-1_short':"metaSPAdes",'Miniasm_GATB_hybrid':"GATB",'OPERA-MS-inhouse_hybrid':"OPERA-MS"}
+    subset = ['ABySS_short','A-STAR_contig_hybrid','HipMer_Metagenome_short','marmgCAMI2_short_read_pooled_gsa','Megahit_v1-1-4-2_short','metaSPAdes_v3-13-1_short','Miniasm_GATB_hybrid','OPERA-MS-inhouse_hybrid']
+else:
+    subset_names = {'ABySS':"ABySS",'A-STAR':"A-STAR",'HipMer':"HipMer",'marmgCAMI2':"gsa",'Megahit':"MEGAHIT",'metaSPAdes':"metaSPAdes",'Miniasm':"GATB",'OPERA-MS-inhouse':"OPERA-MS"}
+    subset = subset_names.keys()
 fig, (ax1, ax2) = plt.subplots(figsize=(14, 12), nrows=1, ncols=2, sharey = True)
 #ax1.set_title("Completeness", fontsize=20)
 ax1.tick_params(labelsize=20)
@@ -103,15 +108,17 @@ ax2.grid(axis='x')
 ax2.set_axisbelow(True)
 plt.setp(ax1.collections, alpha=1)
 plt.setp(ax2.collections, alpha=1)
-fig.suptitle("16S rRNA gene assembly quality", fontsize=30)
-plt.savefig("16S_violins.png", bbox_inches='tight')
-##fig.suptitle("CRISPR cassette assembly quality", fontsize=30)
-##plt.savefig("CRISPR_violins.png", bbox_inches='tight')
+if which == "16S":
+    fig.suptitle("16S rRNA gene assembly quality", fontsize=30)
+    plt.savefig("16S_violins.png", bbox_inches='tight')
+else:
+    fig.suptitle("CRISPR cassette assembly quality", fontsize=30)
+    plt.savefig("CRISPR_violins.png", bbox_inches='tight')
 plt.close()
 
 plot3 = plt.boxplot([lengths[x] for x in subset_names], vert=False, patch_artist=True, zorder=3)
-plt.yticks(np.arange(1, len(subset) + 1),[subset_names[x] for x in subset_names])
-#plt.ticklabel_format(axis='x',style='sci',scilimits=(0,0))
+plt.yticks(np.arange(1, len(subset) + 1),["%s (%s)" % (subset_names[x], len(assemblers[x][1])) for x in subset])
+plt.ticklabel_format(axis='x',style='sci',scilimits=(0,0))
 plt.xlabel("Mapped contig lengths (bp)")
 plt.xscale("log")
 plt.grid(axis='x',zorder=0)
@@ -119,7 +126,9 @@ plt.rc('axes', axisbelow=True)
 colors = sns.color_palette("pastel")
 for patch, color in zip(plot3['boxes'], colors):
     patch.set(facecolor = color)#, alpha = 0.3)
-plt.title("16S rRNA gene-carrying contig lengths", fontsize=12)
-plt.savefig("16S_boxes_lengths.png", dpi=200, bbox_inches='tight')
-##plt.title("CRISPR cassette-carrying contig lengths", fontsize=12)
-##plt.savefig("CRISPR_boxes_lengths.png", dpi=200, bbox_inches='tight')
+if which == "16S":
+    plt.title("16S rRNA gene-carrying contig lengths", fontsize=12)
+    plt.savefig("16S_boxes_lengths.png", dpi=200, bbox_inches='tight')
+else:
+    plt.title("CRISPR cassette-carrying contig lengths", fontsize=12)
+    plt.savefig("CRISPR_boxes_lengths.png", dpi=200, bbox_inches='tight')
